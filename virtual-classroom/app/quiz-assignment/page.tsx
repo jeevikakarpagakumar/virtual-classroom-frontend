41b82b0afc1b287e047c9909a91991a582e66444
jeevikakarpagakumar <132480786+jeevikakarpagakumar@users.noreply.github.com>
2025-04-02 23:42:38 +0530

    Update page.tsx

--- virtual-classroom/app/quiz-assignment/page.tsx
+++ virtual-classroom/app/quiz-assignment/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { ArrowLeft } from "lucide-react";
-import { getQuizzes, getAssignments } from "@/app/_utils/api";
+//import { getQuizzes, getAssignments } from "@/app/_utils/api";
 
 interface Quiz {
   quizID: number;
